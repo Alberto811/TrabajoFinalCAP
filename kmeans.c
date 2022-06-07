74d3fe46f8cbde6d00f75a52abf3f7f6eb0ad65c
Alberto <43956932+Alberto811@users.noreply.github.com>
2022-06-07 19:18:28 +0200

    Add files via upload

--- kmeans.c
+++ kmeans.c
@@ -5,21 +5,20 @@
 #include <time.h>
 
 
-#define MAX_ITERATIONS 100
+#define ITERACIONES_MAXIMAS 100
 
-#define THRESHOLD 1e-4
+#define ERROR 1e-4
 
 float *centroidesGlobal;
 float *puntosClusterGlobal;
-float delta_global = THRESHOLD + 1;
-double current_itr_delta_global;
+float deltaGlobal = ERROR + 1;
+double deltaActual;
 void leerDatos(const char *nombreArchivo, int *N, float **puntos)
 {
 	FILE *fptr = fopen(nombreArchivo, "r");
 
 	fscanf(fptr, "%d", N);
 	*puntos = (float *)malloc(((*N) * 3) * sizeof(float));
-
 	for (int i = 0; i < (*N) * 3; i++)
 	{
 		int temp;
@@ -30,7 +29,7 @@ void leerDatos(const char *nombreArchivo, int *N, float **puntos)
 	fclose(fptr);
 }
 
-void writeClusters(const char *nombreArchivo, int N, float *puntosCluster)
+void escribirArchivoClusters(const char *nombreArchivo, int N, float *puntosCluster)
 {
 	FILE *fptr = fopen(nombreArchivo, "w");
 
@@ -42,7 +41,7 @@ void writeClusters(const char *nombreArchivo, int N, float *puntosCluster)
 	fclose(fptr);
 }
 
-void writeCentroids(const char *nombreArchivo, int K, int iteraciones, float *centroides)
+void escribirArchivoCentroides(const char *nombreArchivo, int K, int iteraciones, float *centroides)
 {
 	FILE *fptr = fopen(nombreArchivo, "w");
 
@@ -60,33 +59,21 @@ void writeCentroids(const char *nombreArchivo, int K, int iteraciones, float *ce
 
 
 
-double findEuclideanDistance(float *pointA, float *pointB)
+double calcularDistanciaEuclidiana(float *puntoA, float *puntoB)
 {
   // Function to find the Euclidean distance between two points in 3 dimensional space
-  return sqrt(pow(((double)(*(pointB + 0)) - (double)(*(pointA + 0))), 2) + pow(((double)(*(pointB + 1)) - (double)(*(pointA + 1))), 2) + pow(((double)(*(pointB + 2)) - (double)(*(pointA + 2))), 2));
+  return sqrt(pow(((double)(*(puntoB + 0)) - (double)(*(puntoA + 0))), 2) + pow(((double)(*(puntoB + 1)) - (double)(*(puntoA + 1))), 2) + pow(((double)(*(puntoB + 2)) - (double)(*(puntoA + 2))), 2));
 }
 
-void threadedClustering(int tID, int N, int K, int nHilos, float *puntos, float **puntosCluster, float **centroides, int *iteraciones)
+void ejecucionAlgoritmo(int N, int K, int nHilos, float *puntos, float **puntosCluster, float **centroides, int *iteraciones)
 {
-  // tID            - The unique ID of the thread which executes this function (read only)
-  // N              - Number of data points in the dataset (read only)
-  // K              - Number of clusters to be created (read only)
-  // nHilos    - No. of threads using which the algorithm should be executed (read only)
-  // puntos    - 3 dimensional data points (read only)
-  // puntosCluster - 3 dimensional data points along with the ID of the cluster to which they belong (write)
-  // centroides      - 3 dimensional coordinate values of the centroides of the K cluster (write)
-  // iteraciones - Number of iterations taken to complete the algorithm (write)
-
-
-
-  //printf("Tamanio %d, %d, %d", tamanioPorHilo, inicio, fin);
 
   double minimaDistancia, distanciaActual;
   int *idClusterActual = (int *)malloc(sizeof(int) * N); // The cluster ID to which the data point belong to after each iteration
 
   int contIteracion = 0;
 
-  while ((delta_global > THRESHOLD) && (contIteracion < MAX_ITERATIONS))
+  while ((deltaGlobal > ERROR) && (contIteracion < ITERACIONES_MAXIMAS))
   {
     float *centroideActual = (float *)calloc(K * 3, sizeof(float)); // Coordinates of the centroides which are calculated at the fin of each iteration
     int *contadorCluster = (int *)calloc(K, sizeof(int));              // No. of data points which belongs to each cluster at the fin of each iteration. Initialised to zero
@@ -96,7 +83,7 @@ void threadedClustering(int tID, int N, int K, int nHilos, float *puntos, float 
 
       for (int j = 0; j < K; j++)
       {
-        distanciaActual = findEuclideanDistance((puntos + (i * 3)), (centroidesGlobal + (contIteracion * K + j) * 3));
+        distanciaActual = calcularDistanciaEuclidiana((puntos + (i * 3)), (centroidesGlobal + (contIteracion * K + j) * 3));
         if (distanciaActual < minimaDistancia)
         {
           minimaDistancia = distanciaActual;
@@ -130,20 +117,20 @@ void threadedClustering(int tID, int N, int K, int nHilos, float *puntos, float 
     double current_delta = 0.0;
     for (int i = 0; i < K; i++)
     {
-      current_delta += findEuclideanDistance((centroidesGlobal + (contIteracion * K + i) * 3), (centroidesGlobal + ((contIteracion - 1) * K + i) * 3));
+      current_delta += calcularDistanciaEuclidiana((centroidesGlobal + (contIteracion * K + i) * 3), (centroidesGlobal + ((contIteracion - 1) * K + i) * 3));
     }
 
     // Store the largest delta value among all delta values in all the threads
-      if (current_delta > current_itr_delta_global)
-        current_itr_delta_global = current_delta;
+      if (current_delta > deltaActual)
+        deltaActual = current_delta;
 
 
     contIteracion++;
 
     // Set the global delta value and increment the number of iterations
 
-    delta_global = current_itr_delta_global;
-    current_itr_delta_global = 0.0;
+    deltaGlobal = deltaActual;
+    deltaActual = 0.0;
     (*iteraciones)++;
 
   }
@@ -156,8 +143,6 @@ void threadedClustering(int tID, int N, int K, int nHilos, float *puntos, float 
     puntosClusterGlobal[i * 4 + 2] = puntos[i * 3 + 2];
     puntosClusterGlobal[i * 4 + 3] = (float)idClusterActual[i - 0];
   }
-
-  // printf("After updating the cluster points %d\n", tID);
 }
 
 void kMeans(int N, int K, int nHilos, float *puntos, float **puntosCluster, float **centroides, int *iteraciones)
@@ -175,7 +160,7 @@ void kMeans(int N, int K, int nHilos, float *puntos, float **puntosCluster, floa
   puntosClusterGlobal = *puntosCluster;
 
   // calloc intitalizes the values to zero
-  centroidesGlobal = (float *)calloc(MAX_ITERATIONS * K * 3, sizeof(float));
+  centroidesGlobal = (float *)calloc(ITERACIONES_MAXIMAS * K * 3, sizeof(float));
 
   // Assigning the first K data points to be the centroides of the K clusters
   for (int i = 0; i < K; i++)
@@ -186,39 +171,25 @@ void kMeans(int N, int K, int nHilos, float *puntos, float **puntosCluster, floa
   }
 
 
-    int tID = 1;
-    printf("Thread no. %d created\n", tID);
-    threadedClustering(tID, N, K, nHilos, puntos, puntosCluster, centroides, iteraciones);
+  ejecucionAlgoritmo(N, K, nHilos, puntos, puntosCluster, centroides, iteraciones);
 
-  int centroids_size = (*iteraciones + 1) * K * 3;
-  *centroides = (float *)calloc(centroids_size, sizeof(float));
-  for (int i = 0; i < centroids_size; i++)
+  int tamanioCentroides = (*iteraciones + 1) * K * 3;
+  *centroides = (float *)calloc(tamanioCentroides, sizeof(float));
+  for (int i = 0; i < tamanioCentroides; i++)
   {
     (*centroides)[i] = centroidesGlobal[i];
   }
 
-  printf("Process Completed\n");
-  printf("Number of iterations: %d\n", *iteraciones);
+  printf("Ejecución terminada\n");
+  printf("Número de iteraciones: %d\n", *iteraciones);
   for (int i = 0; i < K; i++)
   {
-    printf("Final centroides:\t(%f, %f, %f)\n", *(*centroides + ((*iteraciones) * K) + (i * 3)), *(*centroides + ((*iteraciones) * K) + (i * 3) + 1), *(*centroides + ((*iteraciones) * K) + (i * 3) + 2));
+    printf("Centroides 3 dimensiones:\t(%f, %f, %f)\n", *(*centroides + ((*iteraciones) * K) + (i * 3)), *(*centroides + ((*iteraciones) * K) + (i * 3) + 1), *(*centroides + ((*iteraciones) * K) + (i * 3) + 2));
   }
 }
 
 int main(int argc, char const *argv[])
 {
-	if (argc < 6)
-	{
-		printf("Less no. of command line arguments\n\n");
-		return 0;
-	}
-	else if (argc > 6)
-	{
-		printf("Too many command line arguments\n\n");
-		return 0;
-	}
-
-	// Correct no. of command line arguments
 
 	const char *conjuntoDatos = argv[1];
 	const int K = atoi(argv[2]);
@@ -226,10 +197,15 @@ int main(int argc, char const *argv[])
 	const char *archivoPuntosCluster = argv[4];
 	const char *archivoCentroides = argv[5];
 
-	int N;								 // Total no. of puntos in the file
-	float *puntos;		 // 2D array of 3 dimensional data points
+	//total de puntos de datos
+  int N;
+  //puntos sacados de los datos de entrada
+	float *puntos;
+  //puntos con el ID del cluster al que pertenece
 	float *puntosCluster; // 2D array of data points along with the ID of the cluster they belong to
-	float *centroides;			 // 2D array of the coordinates of the centroides of clusters
+  //coordenadas de los centroides
+	float *centroides;
+  //iteraciones
 	int iteraciones = 0;
 
 	leerDatos(conjuntoDatos, &N, &puntos);
@@ -240,8 +216,8 @@ int main(int argc, char const *argv[])
 
 	printf("Tiempo total: %lfs\n", (double) (tiempoFinal - tiempoInicial) / CLOCKS_PER_SEC);
 
-	writeClusters(archivoPuntosCluster, N, puntosCluster);
-	writeCentroids(archivoCentroides, K, iteraciones, centroides);
+	escribirArchivoClusters(archivoPuntosCluster, N, puntosCluster);
+	escribirArchivoCentroides(archivoCentroides, K, iteraciones, centroides);
 
 	return 0;
 }
